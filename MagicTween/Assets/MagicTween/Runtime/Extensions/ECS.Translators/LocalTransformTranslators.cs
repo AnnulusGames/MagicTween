08bceb8affde0750f4e8c70f4733b95bd02f44bf
AnnulusGames <annulusgames@gmail.com>
2023-10-26 19:28:15 +0900

    Change: rename Translators of LocalTransform (BREAKING CHANGE)

--- MagicTween/Assets/MagicTween/Runtime/Extensions/ECS.Translators/LocalTransformTranslators.cs
+++ MagicTween/Assets/MagicTween/Runtime/Extensions/ECS.Translators/LocalTransformTranslators.cs
@@ -8,7 +8,7 @@ using MagicTween.Translators;
 namespace MagicTween.Translators
 {
     [BurstCompile]
-    public struct LocalTransformPositionTranslator : ITweenTranslator<float3, LocalTransform>
+    public struct PositionTranslator : ITweenTranslator<float3, LocalTransform>
     {
         public Entity TargetEntity { get; set; }
         
@@ -20,7 +20,39 @@ namespace MagicTween.Translators
     }
 
     [BurstCompile]
-    public struct LocalTransformRotationTranslator : ITweenTranslator<quaternion, LocalTransform>
+    public struct PositionXTranslator : ITweenTranslator<float, LocalTransform>
+    {
+        public Entity TargetEntity { get; set; }
+
+        [BurstCompile] public void Apply(ref LocalTransform component, in float value)
+            => component.Position = new float3(value, component.Position.y, component.Position.z);
+        [BurstCompile] public float GetValue(ref LocalTransform component) => component.Position.x;
+    }
+
+    [BurstCompile]
+    public struct PositionYTranslator : ITweenTranslator<float, LocalTransform>
+    {
+        public Entity TargetEntity { get; set; }
+
+        [BurstCompile]
+        public void Apply(ref LocalTransform component, in float value)
+            => component.Position = new float3(component.Position.x, value, component.Position.z);
+        [BurstCompile] public float GetValue(ref LocalTransform component) => component.Position.y;
+    }
+
+    [BurstCompile]
+    public struct PositionZTranslator : ITweenTranslator<float, LocalTransform>
+    {
+        public Entity TargetEntity { get; set; }
+
+        [BurstCompile]
+        public void Apply(ref LocalTransform component, in float value)
+            => component.Position = new float3(component.Position.x, component.Position.y, value);
+        [BurstCompile] public float GetValue(ref LocalTransform component) => component.Position.z;
+    }
+
+    [BurstCompile]
+    public struct RotationTranslator : ITweenTranslator<quaternion, LocalTransform>
     {
         public Entity TargetEntity { get; set; }
 
@@ -32,7 +64,7 @@ namespace MagicTween.Translators
     }
 
     [BurstCompile]
-    public struct LocalTransformEulerAnglesTranslator : ITweenTranslator<float3, LocalTransform>
+    public struct EulerAnglesTranslator : ITweenTranslator<float3, LocalTransform>
     {
         public Entity TargetEntity { get; set; }
 
@@ -44,7 +76,67 @@ namespace MagicTween.Translators
     }
 
     [BurstCompile]
-    public struct LocalTransformScaleTranslator : ITweenTranslator<float, LocalTransform>
+    public struct EulerAnglesXTranslator : ITweenTranslator<float, LocalTransform>
+    {
+        public Entity TargetEntity { get; set; }
+
+        [BurstCompile]
+        public void Apply(ref LocalTransform component, in float value)
+        {
+            var eulerAngles = MathUtils.ToEulerAngles(component.Rotation);
+            eulerAngles.x = value;
+            component.Rotation = MathUtils.ToQuaternion(eulerAngles);
+        }
+
+        [BurstCompile]
+        public float GetValue(ref LocalTransform component)
+        {
+            return MathUtils.ToEulerAngles(component.Rotation).x;
+        }
+    }
+
+    [BurstCompile]
+    public struct EulerAnglesYTranslator : ITweenTranslator<float, LocalTransform>
+    {
+        public Entity TargetEntity { get; set; }
+
+        [BurstCompile]
+        public void Apply(ref LocalTransform component, in float value)
+        {
+            var eulerAngles = MathUtils.ToEulerAngles(component.Rotation);
+            eulerAngles.y = value;
+            component.Rotation = MathUtils.ToQuaternion(eulerAngles);
+        }
+
+        [BurstCompile]
+        public float GetValue(ref LocalTransform component)
+        {
+            return MathUtils.ToEulerAngles(component.Rotation).y;
+        }
+    }
+
+    [BurstCompile]
+    public struct EulerAnglesZTranslator : ITweenTranslator<float, LocalTransform>
+    {
+        public Entity TargetEntity { get; set; }
+
+        [BurstCompile]
+        public void Apply(ref LocalTransform component, in float value)
+        {
+            var eulerAngles = MathUtils.ToEulerAngles(component.Rotation);
+            eulerAngles.z = value;
+            component.Rotation = MathUtils.ToQuaternion(eulerAngles);
+        }
+
+        [BurstCompile]
+        public float GetValue(ref LocalTransform component)
+        {
+            return MathUtils.ToEulerAngles(component.Rotation).z;
+        }
+    }
+
+    [BurstCompile]
+    public struct ScaleTranslator : ITweenTranslator<float, LocalTransform>
     {
         public Entity TargetEntity { get; set; }
 
@@ -54,20 +146,37 @@ namespace MagicTween.Translators
         [BurstCompile]
         public float GetValue(ref LocalTransform component) => component.Scale;
     }
-
 }
 
 namespace MagicTween.Core
 {
     [BurstCompile]
-    public sealed partial class LocalTransformPositionTranslationSystem : TweenTranslationSystemBase<float3, LocalTransform, LocalTransformPositionTranslator> { }
+    public sealed partial class PositionTranslationSystem : TweenTranslationSystemBase<float3, LocalTransform, PositionTranslator> { }
 
     [BurstCompile]
-    public sealed partial class LocalTransformRotationTranslationSystem : TweenTranslationSystemBase<quaternion, LocalTransform, LocalTransformRotationTranslator> { }
+    public sealed partial class PositionXTranslationSystem : TweenTranslationSystemBase<float, LocalTransform, PositionXTranslator> { }
 
     [BurstCompile]
-    public sealed partial class LocalTransformEulerAnglesTranslationSystem : TweenTranslationSystemBase<float3, LocalTransform, LocalTransformEulerAnglesTranslator> { }
+    public sealed partial class PositionYTranslationSystem : TweenTranslationSystemBase<float, LocalTransform, PositionYTranslator> { }
 
     [BurstCompile]
-    public sealed partial class LocalTransformScaleTranslationSystem : TweenTranslationSystemBase<float, LocalTransform, LocalTransformScaleTranslator> { }
+    public sealed partial class PositionZTranslationSystem : TweenTranslationSystemBase<float, LocalTransform, PositionZTranslator> { }
+
+    [BurstCompile]
+    public sealed partial class RotationTranslationSystem : TweenTranslationSystemBase<quaternion, LocalTransform, RotationTranslator> { }
+
+    [BurstCompile]
+    public sealed partial class EulerAnglesTranslationSystem : TweenTranslationSystemBase<float3, LocalTransform, EulerAnglesTranslator> { }
+
+    [BurstCompile]
+    public sealed partial class EulerAnglesXTranslationSystem : TweenTranslationSystemBase<float, LocalTransform, EulerAnglesXTranslator> { }
+
+    [BurstCompile]
+    public sealed partial class EulerAnglesYTranslationSystem : TweenTranslationSystemBase<float, LocalTransform, EulerAnglesYTranslator> { }
+
+    [BurstCompile]
+    public sealed partial class EulerAnglesZTranslationSystem : TweenTranslationSystemBase<float, LocalTransform, EulerAnglesZTranslator> { }
+
+    [BurstCompile]
+    public sealed partial class ScaleTranslationSystem : TweenTranslationSystemBase<float, LocalTransform, ScaleTranslator> { }
 }
